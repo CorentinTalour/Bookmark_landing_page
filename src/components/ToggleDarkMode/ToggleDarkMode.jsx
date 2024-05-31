f8710e8346db970e327dee44e908b49f2ed1c410
Corentin Talour <corentin.talour@bts-malraux.net>
2024-05-31 17:53:12 +0200

    NavBar responsive

--- src/components/ToggleDarkMode/ToggleDarkMode.jsx
+++ src/components/ToggleDarkMode/ToggleDarkMode.jsx
@@ -1,12 +1,10 @@
 function ToggleDarkMode({handleChange, isDark}) {
     return (
-        <div className="toggle-container">
-            <button
-                id="check"
-                className="btn-primary btn-dark-mode"
-                onClick={handleChange}
-            >{isDark ? "Light mode" : "Dark mode"}</button>
-        </div>
+        <button
+            id="check"
+            className="btn-primary btn-dark-mode"
+            onClick={handleChange}
+        >{isDark ? "Light mode" : "Dark mode"}</button>
     )
 }
 
